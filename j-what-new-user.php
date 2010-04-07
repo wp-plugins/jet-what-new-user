dcef61bc2cdeead281bf140624d9a68da7209b65
milordk <milordk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-07 20:39:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/jet-what-new-user/trunk@226204 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ j-what-new-user.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: Jet What New User
+URI: http://milordk.ru
+Author: Jettochkin
+Author URI: http://milordk.ru
+Plugin URI: http://milordk.ru/r-lichnoe/opyt-l/cms/jet-what-new-user.html
+Donate URI: http://milordk.ru/uslugi.html
+Description: En: Widget for the publication of their status, placing a note in groups, Ru: Виджет для публикации своего статуса, размещения заметки в группах
+Tags: BuddyPress, Wordpress MU, meta, blog, could
+Version: 0.1
+*/
+?>
+<?
+
+class JetWhatnewf extends WP_Widget {
+	function JetWhatnewf() {
+		parent::WP_Widget(false, $name = __('JetWhatnewf','JetWhatnewf') );
+	}
+
+
+function widget($args, $instance) {
+		extract( $args );
+		echo $before_widget;
+		echo $before_title . $instance['title'] . $after_title;
+include('post-form.php');
+		echo $after_widget;
+	}
+ 
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['jvtitle'] = $new_instance['jvtitle'];
+		$instance['jvavatar'] = $new_instance['jvavatar'];
+		return $instance;
+	}
+
+	function form($instance) {
+		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'jvtitle'=>'1','jvavatar'=>'1'));
+		$title = strip_tags( $instance['title']); 
+        $jvtitle = $instance['jvtitle'];
+        $jvavatar = $instance['jvavatar'];
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'buddypress'); ?>
+		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo attribute_escape( stripslashes( $title ) ); ?>" /></label></p>
+				<p><? _e('Show','buddypress'); _e('Title:','buddypress'); ?></p>
+				<p><input class="checkbox" type="checkbox" <?php if ($jvavatar) {echo '"checked"';} ?> id="<? echo $this->get_field_id('jvtitle'); ?>" name="<? echo $this->get_field_name('jvtitle'); ?>" value="1" /></p>
+				<p><? _e('Show','buddypress'); _e('Avatar','buddypress'); ?></p>
+                <p><input class="checkbox" type="checkbox" <?php if ($jvavatar) {echo '"checked"';} ?> id="<? echo $this->get_field_id('jvavatar'); ?>" name="<? echo $this->get_field_name('jvavatar'); ?>" value="1" /></p>
+	<?php
+	}
+}
+add_action('widgets_init', create_function('', 'return register_widget("JetWhatnewf");'));
+?>
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== Jet Blog Meta List ===
+Contributors: Jettochkin
+Donate link: http://milordk.ru/uslugi.html
+Plugin link: http://milordk.ru/r-lichnoe/opyt-l/cms/jet-what-new-user.html
+Tags: BuddyPress, Wordpress MU, meta, widget, activity
+Requires at least: WP MU 2.8.4a, 1.2.3
+Tested up to: WP MU 2.9.2, BP 1.2.3
+Stable tag: trunk
+
+Conclusion random blogs Wordpress MU as a cloud blogging 
+
+== Description ==
+
+En: Widget for the publication of their status, placing a note in groups
+Ru: Виджет для публикации своего статуса, размещения заметки в группах
+
+== Installation ==
+
+1. Upload `j-what-new-user.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress MU
+3. Infuse field 
+4. Use the widget to sidebar
+
+1. Загрузите `j-what-new-user.php` в директорию `/wp-content/plugins/`
+2. Активируйте плагин в меню `Плагины` Wordpress MU
+3. Установите параметры
+4. Используйте виджет в Сайдбаре
+
+== Contact ==
+
+en: For suggestions, bugs, hugs and love can be donated at the following locations.
+ru: Для предложений, сообщений об ошибках используйте [Plugin Page](http://milordk.ru/r-lichnoe/opyt-l/cms/jet-what-new-user.html)
+
+== Changelog ==
+
+= 0.1 =
+* First Release.
+
+
+`<?php code(); // goes in backticks ?>`
