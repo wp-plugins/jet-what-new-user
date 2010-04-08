2c0bcf0bd9542180f75aa640da25dd462766af04
milordk <milordk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-08 17:57:01 +0000

    add new functions
    git-svn-id: https://plugins.svn.wordpress.org/jet-what-new-user/trunk@226537 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- j-what-new-user.php
+++ j-what-new-user.php
@@ -8,7 +8,7 @@ Plugin URI: http://milordk.ru/r-lichnoe/opyt-l/cms/jet-what-new-user.html
 Donate URI: http://milordk.ru/uslugi.html
 Description: En: Widget for the publication of their status, placing a note in groups, Ru: Виджет для публикации своего статуса, размещения заметки в группах
 Tags: BuddyPress, Wordpress MU, meta, blog, could
-Version: 0.2
+Version: 0.3
 */
 ?>
 <?
@@ -22,8 +22,158 @@ class JetWhatnewf extends WP_Widget {
 function widget($args, $instance) {
 		extract( $args );
 		echo $before_widget;
-		echo $before_title . $instance['title'] . $after_title;
+		$keyavatar = $instance['jvavatar'];
+		$keytitle = $instance['jvtitle'];
+	if ($keytitle)
+		{
+			echo $before_title . $instance['title'] . $after_title;
+		}
+		else
+		{
+		echo $before_title;
+		?>
+			<?php if ( bp_is_group() ) : ?>
+			    <?php printf( __( "What's new in %s, %s?", 'buddypress' ), bp_get_group_name(), bp_get_user_firstname() ) ?>
+			<?php else : ?>
+			    <?php printf( __( "What's new %s?", 'buddypress' ), bp_get_user_firstname() ) ?>
+			<?php endif; ?>
+		<? echo $after_title; 
+		}
+		
+
 /* Start */ ?>
+<? /* With Avatar */ ?>
+<? if ($keyavatar) { ?>
+  <style type="text/css">
+form#whats-new-form {
+	margin-bottom: 5px;
+	border-bottom: 1px solid #f0f0f0;
+	overflow: hidden;
+	padding-bottom: 20px;
+}
+	#item-body form#whats-new-form {
+		margin-top: 20px;
+		border: none;
+	}
+
+	.home-page form#whats-new-form {
+		border-bottom: none;
+		padding-bottom: 0;
+	}
+
+	form#whats-new-form h5 {
+		margin: 0;
+		font-weight: normal;
+		font-size: 12px;
+		color: #888;
+		margin-left: 76px;
+		padding: 0 0 3px 0;
+	}
+
+	form#whats-new-form #whats-new-avatar {
+		float: left;
+	}
+
+	form#whats-new-form #whats-new-content {
+		margin-left: 54px;
+		padding-left: 22px;
+	}
+
+	form#whats-new-form #whats-new-textarea {
+		padding: 8px;
+		border: 1px inset #ccc;
+		background: #fff;
+		margin-bottom: 10px;
+		-moz-border-radius: 3px;
+		-webkit-border-radius: 3px;
+		border-radius: 3px;
+	}
+	form#whats-new-form textarea {
+		width: 100%;
+		height: 60px;
+		font-size: 14px;
+		font-family: inherit;
+		color: #555;
+		border: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	form#whats-new-form #whats-new-options select {
+		max-width: 200px;
+	}
+
+	form#whats-new-form #whats-new-submit {
+		float: left;
+		margin: 0;
+	}
+  </style>
+<? /* With Avatar */ ?>
+<? } else { ?>
+<? /* Without Avatar */ ?>
+  <style type="text/css">
+form#whats-new-form {
+	margin-bottom: 5px;
+	border-bottom: 1px solid #f0f0f0;
+	overflow: hidden;
+	padding-bottom: 20px;
+}
+	#item-body form#whats-new-form {
+		margin-top: 20px;
+		border: none;
+	}
+
+	.home-page form#whats-new-form {
+		border-bottom: none;
+		padding-bottom: 0;
+	}
+
+	form#whats-new-form h5 {
+		margin: 0;
+		font-weight: normal;
+		font-size: 12px;
+		color: #888;
+		margin-left: 76px;
+		padding: 0 0 3px 0;
+	}
+
+	form#whats-new-form #whats-new-content {
+		margin-left: 5px;
+		padding-left: 5px;
+	}
+
+	form#whats-new-form #whats-new-textarea {
+		padding: 8px;
+		border: 1px inset #ccc;
+		background: #fff;
+		margin-bottom: 10px;
+		-moz-border-radius: 3px;
+		-webkit-border-radius: 3px;
+		border-radius: 3px;
+	}
+	form#whats-new-form textarea {
+		width: 100%;
+		height: 60px;
+		font-size: 14px;
+		font-family: inherit;
+		color: #555;
+		border: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	form#whats-new-form #whats-new-options select {
+		max-width: 200px;
+	}
+
+	form#whats-new-form #whats-new-submit {
+<? /*		align: left;*/?>
+		margin: 0;
+	}
+  </style>
+<? /* Without Avatar */ ?>
+<? } ?>
+
 <form action="<?php bp_activity_post_form_action() ?>" method="post" id="whats-new-form" name="whats-new-form">
 
 	<?php do_action( 'bp_before_activity_post_form' ) ?>
@@ -33,20 +183,14 @@ function widget($args, $instance) {
 			<p><?php printf( __( 'You are mentioning %s in a new update, this user will be sent a notification of your message.', 'buddypress' ), bp_get_mentioned_user_display_name( $_GET['r'] ) ) ?></p>
 		</div>
 	<?php endif; ?>
-
+<?	if ($keyavatar)
+		{ ?>
 	<div id="whats-new-avatar">
 		<a href="<?php echo bp_loggedin_user_domain() ?>">
 			<?php bp_loggedin_user_avatar( 'width=60&height=60' ) ?>
 		</a>
 	</div>
-
-	<h5>
-		<?php if ( bp_is_group() ) : ?>
-			<?php printf( __( "What's new in %s, %s?", 'buddypress' ), bp_get_group_name(), bp_get_user_firstname() ) ?>
-		<?php else : ?>
-			<?php printf( __( "What's new %s?", 'buddypress' ), bp_get_user_firstname() ) ?>
-		<?php endif; ?>
-	</h5>
+<? } ?>
 
 	<div id="whats-new-content">
 		<div id="whats-new-textarea">
@@ -86,12 +230,11 @@ function widget($args, $instance) {
 	<?php do_action( 'bp_after_activity_post_form' ) ?>
 
 </form><!-- #whats-new-form -->
-
 <?
 /* End */
-		echo $after_widget;
+			echo $after_widget;
 	}
- 
+
 	function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
@@ -108,10 +251,19 @@ function widget($args, $instance) {
 		?>
 		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'buddypress'); ?>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo attribute_escape( stripslashes( $title ) ); ?>" /></label></p>
-				<p><?php _e('Show','buddypress'); _e('Title:','buddypress'); ?></p>
-				<p><input class="checkbox" type="checkbox" <?php if ($jvavatar) {echo '"checked"';} ?> id="<?php echo $this->get_field_id('jvtitle'); ?>" name="<?php echo $this->get_field_name('jvtitle'); ?>" value="1" /></p>
-				<p><?php _e('Show','buddypress'); _e('Avatar','buddypress'); ?></p>
-                <p><input class="checkbox" type="checkbox" <?php if ($jvavatar) {echo '"checked"';} ?> id="<?php echo $this->get_field_id('jvavatar'); ?>" name="<?php echo $this->get_field_name('jvavatar'); ?>" value="1" /></p>
+		
+<p><?php if ( WPLANG == 'ru_RU' ) { 
+            echo 'Показывать заголовок:';
+        }else{
+                _e('Show'); _e( ' ' );  _e('Title:','buddypress');
+        } ?></p>
+		<p><input class="checkbox" type="checkbox" <?php if ($jvtitle) {echo 'checked="checked"';} ?> id="<?php echo $this->get_field_id('jvtitle'); ?>" name="<?php echo $this->get_field_name('jvtitle'); ?>" value="1" /></p>
+<p><?php if ( WPLANG == 'ru_RU' ) { 
+            echo 'Показывать аватар:';
+        }else{
+                _e('Show'); _e( ' ' );  _e('Avatar','buddypress');
+        } ?></p>		
+		<p><input class="checkbox" type="checkbox" <?php if ($jvavatar) {echo 'checked="checked"';} ?> id="<?php echo $this->get_field_id('jvavatar'); ?>" name="<?php echo $this->get_field_name('jvavatar'); ?>" value="1" /></p>
 	<?php
 	}
 }
--- readme.txt
+++ readme.txt
@@ -3,29 +3,40 @@ Contributors: Jettochkin
 Donate link: http://milordk.ru/uslugi.html
 Plugin link: http://milordk.ru/r-lichnoe/opyt-l/cms/jet-what-new-user.html
 Tags: BuddyPress, Wordpress MU, meta, widget, activity
-Requires at least: WP MU 2.8.4a, 1.2.3
+Requires at least: WP MU 2.8.4a, BP 1.2.3
 Tested up to: WP MU 2.9.2, BP 1.2.3
 Stable tag: trunk
 
-Conclusion random blogs Wordpress MU as a cloud blogging 
+Widget for the publication of their status, placing a note in groups 
+
 
 == Description ==
 
 En: Widget for the publication of their status, placing a note in groups
 Ru: Виджет для публикации своего статуса, размещения заметки в группах
 
+
 == Installation ==
+
 EN
-- Upload `j-what-new-user.php` to the `/wp-content/plugins/` directory
-- Activate the plugin through the 'Plugins' menu in WordPress MU
-- Infuse field 
-- Use the widget to sidebar
+- Upload `j-what-new-user.php` to the `/wp-content/plugins/` directory,
+
+- Activate the plugin through the 'Plugins' menu in WordPress MU,
+
+- Infuse field,
+
+- Use the widget to sidebar.
+
 
 RU
-- Загрузите `j-what-new-user.php` в директорию `/wp-content/plugins/`
-- Активируйте плагин в меню `Плагины` Wordpress MU
-- Установите параметры
-- Используйте виджет в Сайдбаре
+- Загрузите `j-what-new-user.php` в директорию `/wp-content/plugins/`,
+
+- Активируйте плагин в меню `Плагины` Wordpress MU,
+
+- Установите параметры,
+
+- Используйте виджет в Сайдбаре.
+
 
 == Contact ==
 
@@ -34,8 +45,11 @@ ru: Для предложений, сообщений об ошибках исп
 
 == Changelog ==
 
+= 0.3 =
+* Add select avatar/title show
+
 = 0.2 =
-Fixed any bug's
+* Fixed some bug's
 
 = 0.1 =
 * First Release.
